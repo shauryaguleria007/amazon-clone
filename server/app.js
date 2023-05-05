c6035b5c9cf7c5f3edac8094b01c2c5bb75b1ec6
shaurya guleria <shauryaguleria12345678@gmail.com>
2023-05-05 21:56:06 +0530

    authentication

--- server/app.js
+++ server/app.js
@@ -1,29 +1,37 @@
-var express=require('express');
-const mongoose= require('mongoose');
-const mongoDB=process.env.MONGODB_URL||"mongodb+srv://itssagar11:0135india@cluster0.4yxhpzs.mongodb.net/library?retryWrites=true&w=majority://localhost:27017/mongodb+srv://itssagar11:0135Indi@@cluster0.mdd4cvd.mongodb.net/?libraryretryWrites=true";
+var express = require('express');
+require("dotenv").config()
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const mongoDB = process.env.MONGODB_URL || "mongodb+srv://itssagar11:0135india@cluster0.4yxhpzs.mongodb.net/library?retryWrites=true&w=majority://localhost:27017/mongodb+srv://itssagar11:0135Indi@@cluster0.mdd4cvd.mongodb.net/?libraryretryWrites=true";
 
 
-var app=express();
 
-var indexRoute=require('./routes/index.js');
-var authRouter=require('./routes/auth.js');
+var app = express();
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials: true,
+  methods: ['GET', 'PUT', 'POST', 'OPTIONS'],
+}))
+var indexRoute = require('./routes/index.js');
+var authRouter = require('./routes/auth.js');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // app.use(cookieParser());
 
-app.use('/',indexRoute);
+app.use('/', indexRoute);
 
-app.use('/auth',authRouter);
-
-
-mongoose.connect(mongoDB,{ useNewUrlParser: true, w: "majority" })
-.then((res)=>{
-  console.log("connected to database")
-})
-.catch((err)=>{console.log(err)});
+app.use('/auth', authRouter);
 
 
-app.listen(3000,()=>{
-    console.log("Server Starts on 3000");
+mongoose.connect(mongoDB, { useNewUrlParser: true, w: "majority" })
+  .then((res) => {
+    console.log("connected to database")
+  })
+  .catch((err) => { console.log(err) });
+
+
+app.listen(3000, () => {
+  console.log("Server Starts on 3000");
 });
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,117 +2,115 @@ require('dotenv').config()
 var express = require('express');
 var router = express.Router();
 
-const mongoose= require("mongoose");
-const user= require("../models/user");
-const validation= require("express-validator");
-const async=require("async");
-const bcrypt=require("bcrypt");
-const passport=require("passport");
-const jwt=require("jsonwebtoken");
-const JWTStrategy=require("passport-jwt").Strategy; 
-const {ExtractJwt}=require("passport-jwt");
-const check=validation.body;
-const validateResult= validation.validationResult;
+const mongoose = require("mongoose");
+const user = require("../models/user");
+const validation = require("express-validator");
+const async = require("async");
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const jwt = require("jsonwebtoken");
+const JWTStrategy = require("passport-jwt").Strategy;
+const { ExtractJwt } = require("passport-jwt");
+const check = validation.body;
+const validateResult = validation.validationResult;
 
 passport.use(new JWTStrategy({
-    jwtFromRequest:ExtractJwt.fromAuthHeaderAsBearerToken(),
-    secretOrKey:process.env.SECRET_KEY,
-  },(jwtPayload,cb)=>{
-  
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: process.env.SECRET_KEY,
+}, (jwtPayload, cb) => {
+
     return cb(null, jwtPayload);
-  }));
+}));
 
 
 router.post("/signup", [
+    check('fullname', 'Name Required').trim().isLength({ min: 2 }).escape(),
+    check('mobile', 'Phone No  Required').trim().isMobilePhone().escape(),
+    check('password', 'Password  Required').trim().isLength({ min: 6 }).withMessage("Minimum Password Length is 6").escape(),
+    (req, res, next) => {
+        const errors = validateResult(req);
 
- 
-    check('fullname','Name Required').trim().isLength({min:2}).escape(),
-    check('mobile','Phone No  Required').trim().isMobilePhone().escape(),
-    check('password','Password  Required').trim().isLength({min:6}).withMessage("Minimum Password Length is 6").escape(),
-    (req,res,next)=>{
-        const errors= validateResult(req);
-
-        if(!errors.isEmpty()){
+        if (!errors.isEmpty()) {
             res.status(400).send(errors);
             return;
-        }else{
-            user.findOne({ mobile: req.body.mobile }).exec().then(function(doc) {
+        } else {
+            user.findOne({ mobile: req.body.mobile }).exec().then(function (doc) {
                 if (doc) {
                     return res.status(400).json({ message: 'Mobile number already exists' });
                 }
-                const obj= new user({
-                    name:req.body.fullname  ,
-                    mobile:req.body.mobile ,
-                    password:req.body.password
+                const obj = new user({
+                    name: req.body.fullname,
+                    mobile: req.body.mobile,
+                    password: req.body.password
                 });
-                obj.save().then(function() {
+                obj.save().then(function () {
                     res.status(200).json({ message: 'User registered' });
-                }).catch(function(err) {
+                }).catch(function (err) {
                     res.status(500).json({ message: err.message });
                 });
                 // continue with the rest of your code
-            }).catch(function(err) {
+            }).catch(function (err) {
                 return next(err);
             })
         }
-    
+
     }
 
 ]);
 
 
 
-router.post("/signin" ,[
-    check('mobile','Mobile No Required').trim().escape(),
-    check('password','Password  Required').trim().escape(),
-    async (req,res,next)=>{
-            console.log(req.body);
-           
-        const err= validateResult(req);
-        if(!err.isEmpty()){
+router.post("/signin", [
+    check('mobile', 'Mobile No Required').trim().escape(),
+    check('password', 'Password  Required').trim().escape(),
+    async (req, res, next) => {
+
+        const err = validateResult(req);
+        if (!err.isEmpty()) {
             res.status(400).send(err);
             return;
         }
-        const u=   await user.findOne({mobile:req.body.mobile});
-        if(u==null){
-            res.status(409).json({message:"Credential Invalid"});
+        const u = await user.findOne({ mobile: req.body.mobile });
+        if (u == null) {
+            res.status(409).json({ message: "Credential Invalid" });
             return
-        }else{
-            let hash= await bcrypt.compare(req.body.password,u.password);
-            if(hash){
+        } else {
+            let hash = await bcrypt.compare(req.body.password, u.password);
+            if (hash) {
                 // user verified
-                const payload={
-                    sub:u.id,
-                    iat:Date.now()
+                const payload = {
+                    sub: u.id,
+                    iat: Date.now()
                 }
-                const token=  await jwt.sign(payload,process.env.SECRET_KEY,{expiresIn: "2h",});
+                const token = await jwt.sign(payload, process.env.SECRET_KEY, { expiresIn: "2h", });
                 // res.setHeader("Set-Cookie",cookie.serialize('access-token',token,{
                 //     // httpOnly:true // disable for testing
                 //     sameSite:'strict',
                 //     maxAge:180000
                 // })) //vulnulrable 
                 return res.status(200).send({
-                    message:"Successfully Login",
-                    user:u.username,
+                    message: "Successfully Login",
+                    user: u.username,
                     token
                 })
 
-            }else{
-                res.status(409).json({message:"Credential Invalid"});
+            } else {
+                res.status(409).json({ message: "Credential Invalid" });
                 return;
             }
         }
 
-           
-    
+
+
     }
 ])
 
 
 
-router.get("/protected", passport.authenticate('jwt', { session: false }), (req, res) => {
-    res.send("You are authorized to access this protected resource!");
-  });
-  
+router.get("/protected", passport.authenticate('jwt', { session: false }), async (req, res) => {
+    const account = await user.findById(req.user.sub).select(["-password"])
+    res.json(account);
+});
 
-module.exports=router;
+
+module.exports = router;
