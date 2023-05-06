6cfc536a91cab9b4f31c3a534c461cadb3d8ea1b
shaurya guleria <shauryaguleria12345678@gmail.com>
2023-05-06 10:09:56 +0530

    server

--- frontend/src/components/Authorizer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import { Loading} from "./Loading"
-
-export const Authorizer = () => {
-    //authenticate ,id failed navigate to login 
-    return <Outlet />
-    return <Loading />
-}
